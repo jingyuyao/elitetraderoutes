6c33ff761d6dc1c0e96da24b23682676e75fda69
Jingyu Yao <yjy.emails@gmail.com>
2015-06-26 23:40:16 -0400

    Bug fixes for connection creation.
    Gahh, stopped using filters to create inputs.

--- frontend/static/frontend/js/main.js
+++ frontend/static/frontend/js/main.js
@@ -12,61 +12,86 @@ var endpointToInputs = {
 
 var cache = {};
 
-function matcher(item){
-    return this.query.toLowerCase().indexOf(item.name) != -1;
+function iCompare(search, source){
+    return source.toLowerCase().indexOf(search.toLowerCase()) != -1;
 }
 
-function attachInputToModel(name, model){
-    var input = '#' + name + '_input';
-    var isStation = name.search('station') != -1;
+function findModel(name){
+    if (iCompare('system', name)){
+        return 'systems';
+    }
 
-    $(input).typeahead({
-        minLength: isStation ? 0 : 2,
-        showHintOnFocus: isStation,
-        delay: 250, // Millisecond
-        source: function(query, process) {
-          var requestUrl = '/' + model + '/?search=' + query;
+    if (iCompare('station', name)){
+        return 'stations';
+    }
 
-          // If the input is for a station, only supply data from the corresponding system.
-          // If no system is selected, return a helper message first.
-          if (isStation){
-              // Return cached station data for this input if it exists
-              if (cache.hasOwnProperty(name)){
-                  process(cache[name]);
-                  return;
-              }
+    if (iCompare('commodity', name)){
+        return 'commodities';
+    }
+}
 
-              var correspondingSystemInput = input.replace('station', 'system');
-              var selectedSystem = $(correspondingSystemInput).data('selected');
-              if (typeof selectedSystem !== 'undefined'){
-                requestUrl = selectedSystem.url + 'stations/';
-              }
-              else{
-                process([]);
-                return;
-              }
-            }
+function attachInputToModel(){
+    $('input').each(function(){
+        var $input = $(this);
+        var name = $input.attr('name');
+        var model = findModel(name);
 
-            $.getJSON(requestUrl, function(response){
-              var autocompleteList = response.data.results;
+        if (typeof model === 'undefined'){
+            return;
+        }
+
+        var isStation = name.search('station') != -1;
+
+        $input.typeahead({
+            minLength: isStation ? 0 : 2,
+            showHintOnFocus: isStation,
+            delay: 250, // Millisecond
+            source: function(query, process) {
+              var requestUrl = '/' + model + '/?search=' + query;
+
+              // If the input is for a station, only supply data from the corresponding system.
+              // If no system is selected, return a helper message first.
               if (isStation){
-                // Cache the list of station results for this input
-                cache[name] = autocompleteList;
-              }
-              process(autocompleteList);
-            });
-        },
-        afterSelect: function(suggestion){
-            $(input).data('selected', suggestion);
-            if (!isStation){
-                // Resets the associated station input data cache when a new
-                // system is selected.
-                var correspondingStationInput = name.replace('system', 'station');
-                if (cache.hasOwnProperty(correspondingStationInput)){
-                    delete cache[correspondingStationInput];
+                  // Return cached station data for this input if it exists
+                  if (cache.hasOwnProperty(name)){
+                      process(cache[name]);
+                      return;
+                  }
+
+                  var correspondingSystemInput = name.replace('station', 'system');
+                  // We got rid of the id for input tags cause there is no easy way to make them unique across multiple
+                  // forms. Now we have to use selectors to find nearest sibling input
+                  var selectedSystem = $input.closest('form').find('input[name=' + correspondingSystemInput + ']').data('selected');
+                  if (typeof selectedSystem !== 'undefined'){
+                    requestUrl = selectedSystem.url + 'stations/';
+                  }
+                  else{
+                    process([]);
+                    return;
+                  }
+                }
+
+                $.getJSON(requestUrl, function(response){
+                  var autocompleteList = response.data.results;
+                  if (isStation){
+                    // Cache the list of station results for this input
+                    cache[name] = autocompleteList;
+                  }
+                  process(autocompleteList);
+                });
+            },
+            afterSelect: function(suggestion){
+                $input.data('selected', suggestion);
+                if (!isStation){
+                    // Resets the associated station input data cache when a new
+                    // system is selected.
+                    var correspondingStationInput = name.replace('system', 'station');
+                    if (cache.hasOwnProperty(correspondingStationInput)){
+                        delete cache[correspondingStationInput];
+                    }
                 }
             }
-        }
+        });
     });
 }
 
@@ -84,7 +109,8 @@ function attachInputsToModels(dict){
 }
 
 $(function(){
-    attachInputsToModels(endpointToInputs);
+//    attachInputsToModels(endpointToInputs);
+    attachInputToModel();
 
     // When submitting a form, replace inputs with input.data('selected').url
     $('form').each(function(){
