c0c0c1d8bdd141968c8908ddbe0c190e2af1bef1
Jingyu Yao <yjy.emails@gmail.com>
2016-01-30 01:28:18 +0000

    Potential fix for build failing

--- project_setup.sh
+++ project_setup.sh
@@ -4,7 +4,8 @@ echo "Migrating.."
 python manage.py migrate
 echo "Testing..."
 python manage.py test
-echo "Download and fixing data..."
-python ingest_data.py
+# EDDB changed their data layout so it is incompatible with our existing database :(
+# echo "Download and fixing data..."
+# python ingest_data.py
 echo "Loading data to database..."
 python manage.py loaddata commodity system station
