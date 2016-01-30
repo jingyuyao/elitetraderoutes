bcff1674e3ff5b544a87ec48d23dc815e7be0627
Jingyu Yao <yjy.emails@gmail.com>
2016-01-30 01:38:18 +0000

    Gave up loadding fresh base data at all. Just going to mysqldump the database if we have to move it...

--- project_setup.sh
+++ project_setup.sh
@@ -7,5 +7,5 @@ python manage.py test
 # EDDB changed their data layout so it is incompatible with our existing database :(
 # echo "Download and fixing data..."
 # python ingest_data.py
-echo "Loading data to database..."
-python manage.py loaddata commodity system station
+# echo "Loading data to database..."
+# python manage.py loaddata commodity system station
